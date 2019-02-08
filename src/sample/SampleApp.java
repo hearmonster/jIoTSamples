18fb910ffc11f40677bf3585f2449b894c616567
Paul Hearmon <HearMonster@gmail.com>
2019-02-08 13:02:09 -0600

    Added comments
    Describes (obfuscated) initialization sequence in detail

--- src/sample/SampleApp.java
+++ src/sample/SampleApp.java
@@ -27,6 +27,43 @@ import commons.utils.SecurityUtil;
 
 public class SampleApp
 extends AbstractCoreServiceSample {
+	//Upon instantiation, this object *extends* the 'AbstractCoreServiceSample' Class, which in turn...
+	//*extends* itself with 'AbstractSample'
+	// So the order of initialization is actually:
+	//	1. AbstractSample
+	//	2. AbstractCoreServiceSample
+	//	3. SampleApp
+
+	// 1 - AbstractSample
+	// a) establishes the following Class attributes:
+	//	sets up a bunch on constants that relate to each property key (e.g IOT_HOST = "iot.host" etc)
+	// b) The Constructor prints a bunch of lines (nothing special), then calls its 'init()' method
+	// c) The 'init()' method loads in the Properties, and sets the proxy
+	
+	// 2 - AbstractCoreServiceSample
+	// d) establishes the following Class attribute objects:
+	//		CoreService coreService;
+	//		ProcessingService processingService;
+	//		Comparator<SensorTypeCapability> sensorTypeCapabilityComparator;
+	// e) The Constructor establishes the following string values:
+	//		host, instance, tenant, user, password
+	// f) Then initializes the following services:
+	// 		coreService = new CoreService(host, instance, tenant, user, password);
+	//		processingService = new ProcessingService(host, tenant, user, password);
+	//		sensorTypeCapabilityComparator = Comparator.comparing(SensorTypeCapability::getId);
+	// g) The Constructor of 'CoreServices' [used to create the conn, then create the artifacts]  establishes
+	//		baseUri
+	//		httpClient
+	// h) The Constructor of 'ProcessingServices' [used to publish the readings] establishes *exactly the same* inc. same base URL
+	//		baseUri
+	//		httpClient
+	// i) The Constructor of 'java.util.Comparator' is unknown to me
+	
+	// 3 - SampleApp
+	// j) establishes the following Class attribute objects:
+	//		GatewayCloud gatewayCloud;
+	// (no Constructor for SampleApp)
+
 
 	private GatewayCloud gatewayCloud;
 
@@ -40,19 +77,41 @@ extends AbstractCoreServiceSample {
 	throws SampleException {
 		String deviceId = properties.getProperty(DEVICE_ID);
 		String sensorId = properties.getProperty(SENSOR_ID);
+
+		//get the respective 'Gateway Protocol' object (HTTP Gateway or MQTT Gateway) given the 'gateway.protocol.id' property
 		GatewayProtocol gatewayProtocol = GatewayProtocol.fromValue(properties.getProperty(GATEWAY_PROTOCOL_ID));
 
 		try {
 			Console.printSeparator();
 
+			//get the respective Gateway object (HTTP Gateway or MQTT Gateway) given the 'gatewayProtocol' object
 			Gateway gateway = coreService.getOnlineCloudGateway(gatewayProtocol);
 
 			Console.printSeparator();
 
+			//get the Device's java object instance, given the DeviceID property and Gateway object
+
+			//Takes a DeviceID (that may or may not be valid, or might even be null!) and a Gateway object (HTTP or MQTT)
+			// Attempt to (safely) look up the device given the ID
+			//If it finds an existing one in the IoT Core Service, then simply return the Java object instance. 
+			// Any error (due to a nonexistent/null ID) will simply 
+			//	a) trigger a warning
+			//	b) call 'ArtifactFactory.buildSampleDevice' to create an instance within the IoT Core Service
+			//	c) print out the DeviceID to the log
+
+			// TODO add the new DeviceID to the properties object bag?
+			// TODO persist the new DeviceID to the properties file
 			Device device = getOrAddDevice(deviceId, gateway);
 
 			Console.printSeparator();
 
+			//Build a new Capability java object instance, but only in memory, named "Ambient" (with an AlternateID of "ambient")
+			//that comprises of three properties; Humidity, Temperature, Light
+			
+			//It's used as a template.  Having created an instance of one (in memory) 
+			//  we pass it to 'AbstractDeviceInstanceCoreServices.getOrAddCapability'
+			//Who in turn, searches for a copy of it (through the list of *all* the existing/persisted Capabilities in the IoT Core Service), 
+			// and either returns the existing/persisted instance or creates a new one (using 'coreService.addCapability')
 			Capability measureCapability = getOrAddCapability(EntityFactory.buildAmbientCapability());
 
 			Console.printSeparator();
@@ -67,8 +126,11 @@ extends AbstractCoreServiceSample {
 
 			Console.printSeparator();
 
+			// Create an Authentication (a Secret, a combined Private Key + Certificate PEM, and a Password), for a given a Device object instance 
 			Authentication authentication = coreService.getAuthentication(device);
 
+			// Create a PKCS12 Keystore from the PEM
+			// Create the 'Key Manager' and 'Trust Manager' components necessary *prior* to establishing an SSL link 
 			SSLSocketFactory sslSocketFactory = SecurityUtil.getSSLSocketFactory(device, authentication);
 
 			switch (gatewayProtocol) {
